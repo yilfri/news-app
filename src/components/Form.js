8a821d6496dc68aadaab15c95546a046fa424fd4
yilfri <yilfri97@hotmail.com>
2021-05-31 20:36:00 -0400

    show categories in IU

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,25 @@ import styles from './Form.module.css';
 import useSelect from '../hooks/useSelect';
 
 const Form = () => {
-	const [category, NewsSelect] = useSelect();
+	const OPTIONS = [
+		{ value: 'general', label: 'General' },
+		{ value: 'business', label: 'Negocios' },
+		{ value: 'entertainment', label: 'Entretenimiento' },
+		{ value: 'health', label: 'Salud' },
+		{ value: 'science', label: 'Ciencia' },
+		{ value: 'sport', label: 'Deportes' },
+		{ value: 'technology', label: 'Tecnología' }
+	];
+
+	const [category, NewsSelects] = useSelect('general', OPTIONS);
+
 	return (
 		<div className={`${styles.heading} row`}>
 			<div className="col s12 m8 offset-m2">
 				<form>
 					<h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+					<NewsSelects />
 
 					<div className="input-field col s12">
 						<input
--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
 const useSelect = (initialState, options) => {
-	const [state, setState] = useState('');
+	const [state, setState] = useState(initialState);
 
-	const NewsSelect = () => {
-		<select className="browser-default">
-			<option value="">-- Seleccionar --</option>
-		</select>;
-	};
+	const NewsSelect = () => (
+		<select className="browser-default" value={state} onChange={(e) => setState(e.target.value)}>
+			{options.map((option) => (
+				<option key={option.value} value={option.value}>
+					{option.label}
+				</option>
+			))}
+		</select>
+	);
 
 	return [state, NewsSelect];
 };
