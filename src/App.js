7a49f8c21907cee277ab4bfe409e2a0ed46528e9
yilfri <yilfri97@hotmail.com>
2021-05-31 20:52:55 -0400

    fetch to api news

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 import Header from './components/Header';
 
 function App() {
 	const [category, setCategory] = useState('');
+	const [articles, setArticles] = useState({});
+
+	useEffect(() => {
+		const getAPI = async () => {
+			const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${category}&apiKey=4e9b5fd6ef5b4bdba32acb42fd31a652`;
+
+			const data = await fetch(url);
+			const result = await data.json();
+
+			setArticles(result.articles);
+		};
+
+		getAPI();
+	}, [category]);
+
 	return (
 		<>
 			<Header title="Buscador de Noticias" />
