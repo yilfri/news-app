081aef7a1b1ce4f1504b7c9ea50f48d2b55fb010
yilfri <yilfri97@hotmail.com>
2021-06-01 23:02:27 -0400

    fix NewsList Component

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,13 +2,17 @@ import React from 'react';
 import News from './News';
 import PropTypes from 'prop-types';
 
-const NewsList = ({ articles }) => (
-	<div className="row">
-		{articles.map((article) => (
-			<News key={article.url} article={article} />
-		))}
-	</div>
-);
+const NewsList = ({ articles }) => {
+	if (articles.lenght <= 0) return null;
+
+	return (
+		<div className="row">
+			{articles.map((article) => (
+				<News key={article.url} article={article} />
+			))}
+		</div>
+	);
+};
 
 NewsList.propTypes = {
 	articles: PropTypes.array.isRequired
